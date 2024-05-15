eaa8d90f8c2dac81a5219d08d7da37e235ed7d81
Aismail1975 <aismail@polygon-inc.com>
2024-05-15 22:11:48 +0530

    Updated config

--- config/env.config.qa.js
+++ config/env.config.qa.js
@@ -80,6 +80,7 @@ module.exports = {
     "apiZNERELAPSEDTIMESRV":"ZN_ER_ELAPSED_TIME_SRV",
     "apiZNCARTRECEIVESRV":"ZN_CART_RECEIVE_SRV",
     "apiZNNURSEENDORSSRV":"ZN_NURSE_ENDORS_SRV",
+    "apiZNSURGICALPASSPORTSRV":"ZN_SURGICAL_PASSPORT_SRV",
     "apiDomain": "ach.jo",
     "AllowOriginDomain": "http://abdaliwebserver.ach.jo:8090",
     "environment": "qa",
