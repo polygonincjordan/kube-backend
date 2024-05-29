77974002559d088a20a29454240b853da253b19b
Smit Patel <smit.patel007@outlook.com>
2024-05-29 11:37:46 +0530

    .implemented getdialysispdf API

--- src/routes/index.js
+++ src/routes/index.js
@@ -662,7 +662,9 @@ router.get("/Dialysisget", (req, res, next) => {
 router.put("/Dialysisget", (req, res, next) => {
     emergencyController.releaseDialysisDoc(req, res);
 });
-
+router.get("/Dialysisget", (req, res, next) => {
+    emergencyController.getDialysisPDF(req, res);
+});
 router.post("/DailysisSet", (req, res, next) => {
     emergencyController.DailysisSet(req, res);
 })
