f5bd68b0d17c83e25d7ca281cac09bb7ce3b0270
Smit Patel <smit.patel007@outlook.com>
2024-05-29 11:17:27 +0530

    #Resolved release api related issue

--- src/routes/emergency-dashboard.js
+++ src/routes/emergency-dashboard.js
@@ -7185,15 +7185,53 @@ exports.getDailysisSet = (req, res) => {
     })
 }
 
+exports.deleteDailysisSet = (req, res) => {
+    let mysapSSO2Value = decodeURI(req.cookies['MYSAPSSO2']);
+    let mySAPSSO2Cookie = 'MYSAPSSO2=' + decodeURI(mysapSSO2Value);
+
+    var j = request.jar();
+    var cookie = request.cookie('MYSAPSSO2' + '=' + mysapSSO2Value);
+
+    console.log(baseURL + config.apiZNDAILYSISASSESSRV + `/DailysisSet(Dockey='${req.query.Dockey}')`);
+
+    request({
+        method: 'DELETE',
+        uri: baseURL + config.apiZNDAILYSISASSESSRV + `/DailysisSet(Dockey='${req.query.Dockey}')`,
+        json: true,
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'X-Requested-With': 'XMLHttpRequest',
+            'sap-client': config.client,
+            'Cookie': mySAPSSO2Cookie,
+
+            //'Authorization': 'Basic cmFrc2hpdGQ6aWRoYUAxMjM=',
+        }
+    }, function (error, response, body) {
+        if (error) {
+            res.json(error);
+            return console.dir(error);
+        }
+        else {
+            res.header('Access-Control-Allow-Origin', config.AllowOriginDomain);
+            res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+            res.header('Access-Control-Expose-Headers', 'Content-Length');
+            res.header('Access-Control-Allow-Credentials', 'true');
+            res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Origin,sap-client,Accept, Authorization, Content-Type, X-Requested-With, Range,Access-Control-Allow-Credentials');
+            return res.status(response.statusCode).json(body);
+        }
+    })
+}
+
 exports.releaseDialysisDoc = (req, res) => {
     let mysapSSO2Value = decodeURI(req.cookies['MYSAPSSO2']);
     let mySAPSSO2Cookie = 'MYSAPSSO2=' + decodeURI(mysapSSO2Value);
 
     var j = request.jar();
     var cookie = request.cookie('MYSAPSSO2' + '=' + mysapSSO2Value);
-    const urlEndpoint = String.raw`${baseURL}${config.apiZNDAILYSISASSESSRV}/DailysisSet(Dockey='${req.query.Dockey}')`
+    const urlEndpoint = String.raw`${baseURL}${config.apiZNDAILYSISASSESSRV}/DailysisSet`
     request({
-        method: 'PUT',
+        method: 'POST',
         uri:`${urlEndpoint}`,
         body: req.body,
         json: true,
@@ -7221,85 +7259,6 @@ exports.releaseDialysisDoc = (req, res) => {
              if(response.statusCode != 200){
               logger.log('error', `Status Code: ${response.statusCode}\nBody: ${body}\nURL Endpoint: ${urlEndpoint}\nFile Name:emergency-dashboard.js`);
             }
-            return res.status(response.statusCode).json(body);
-        }
-    })
-}
-
-
-
-
-exports.deleteDailysisSet = (req, res) => {
-    let mysapSSO2Value = decodeURI(req.cookies['MYSAPSSO2']);
-    let mySAPSSO2Cookie = 'MYSAPSSO2=' + decodeURI(mysapSSO2Value);
-
-    var j = request.jar();
-    var cookie = request.cookie('MYSAPSSO2' + '=' + mysapSSO2Value);
-
-    console.log(baseURL + config.apiZNDAILYSISASSESSRV + `/DailysisSet(Dockey='${req.query.Dockey}')`);
-
-    request({
-        method: 'DELETE',
-        uri: baseURL + config.apiZNDAILYSISASSESSRV + `/DailysisSet(Dockey='${req.query.Dockey}')`,
-        json: true,
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            'X-Requested-With': 'XMLHttpRequest',
-            'sap-client': config.client,
-            'Cookie': mySAPSSO2Cookie,
-
-            //'Authorization': 'Basic cmFrc2hpdGQ6aWRoYUAxMjM=',
-        }
-    }, function (error, response, body) {
-        if (error) {
-            res.json(error);
-            return console.dir(error);
-        }
-        else {
-            res.header('Access-Control-Allow-Origin', config.AllowOriginDomain);
-            res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-            res.header('Access-Control-Expose-Headers', 'Content-Length');
-            res.header('Access-Control-Allow-Credentials', 'true');
-            res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Origin,sap-client,Accept, Authorization, Content-Type, X-Requested-With, Range,Access-Control-Allow-Credentials');
-            return res.status(response.statusCode).json(body);
-        }
-    })
-}
-
-exports.deleteDailysisSet = (req, res) => {
-    let mysapSSO2Value = decodeURI(req.cookies['MYSAPSSO2']);
-    let mySAPSSO2Cookie = 'MYSAPSSO2=' + decodeURI(mysapSSO2Value);
-
-    var j = request.jar();
-    var cookie = request.cookie('MYSAPSSO2' + '=' + mysapSSO2Value);
-
-    console.log(baseURL + config.apiZNDAILYSISASSESSRV + `/DailysisSet(Dockey='${req.query.Dockey}')`);
-
-    request({
-        method: 'DELETE',
-        uri: baseURL + config.apiZNDAILYSISASSESSRV + `/DailysisSet(Dockey='${req.query.Dockey}')`,
-        json: true,
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            'X-Requested-With': 'XMLHttpRequest',
-            'sap-client': config.client,
-            'Cookie': mySAPSSO2Cookie,
-
-            //'Authorization': 'Basic cmFrc2hpdGQ6aWRoYUAxMjM=',
-        }
-    }, function (error, response, body) {
-        if (error) {
-            res.json(error);
-            return console.dir(error);
-        }
-        else {
-            res.header('Access-Control-Allow-Origin', config.AllowOriginDomain);
-            res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-            res.header('Access-Control-Expose-Headers', 'Content-Length');
-            res.header('Access-Control-Allow-Credentials', 'true');
-            res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Origin,sap-client,Accept, Authorization, Content-Type, X-Requested-With, Range,Access-Control-Allow-Credentials');
             return res.status(response.statusCode).json(body);
         }
     })
