bbd66e5b1155787bfd3a107bf06b89bd5921c216
Smit Patel <smit.patel007@outlook.com>
2024-06-11 11:10:19 +0530

    Implemented all api for hemoDialysis fistula/graft

--- src/routes/emergency-dashboard.js
+++ src/routes/emergency-dashboard.js
@@ -8043,4 +8043,200 @@ exports.copyPediatricWarningScore = (req, res) => {
             return res.status(response.statusCode).json(body);
         }
     })
+
+    exports.fistulaGraftSet = (req, res) => {
+        let mysapSSO2Value = decodeURI(req.cookies['MYSAPSSO2']);
+        let mySAPSSO2Cookie = 'MYSAPSSO2=' + decodeURI(mysapSSO2Value);
+    
+        var j = request.jar();
+        var cookie = request.cookie('MYSAPSSO2' + '=' + mysapSSO2Value);
+        let urlEndpoint = String.raw`${baseURL}` + config.apiFISTULAGRAFTSRV + "/FistulaGraftSet";
+    
+        request({
+            method: 'POST',
+            uri: `${urlEndpoint}`,
+            body: req.body,
+            json: true,
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'X-Requested-With': 'XMLHttpRequest',
+                'sap-client': config.client,
+                'Cookie': mySAPSSO2Cookie,
+    
+                //'Authorization': 'Basic cmFrc2hpdGQ6aWRoYUAxMjM=',
+            }
+        }, function (error, response, body) {
+            if (error) {
+                res.json(error);
+                return console.dir(error);
+            }
+            else {
+                res.header('Access-Control-Allow-Origin', config.AllowOriginDomain);
+                res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+                res.header('Access-Control-Expose-Headers', 'Content-Length');
+                res.header('Access-Control-Allow-Credentials', 'true');
+                res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Origin,sap-client,Accept, Authorization, Content-Type, X-Requested-With, Range,Access-Control-Allow-Credentials');
+                if(response.statusCode != 200){
+                      logger.log('error', `Status Code: ${response.statusCode}\nBody: ${body}\nURL Endpoint: ${urlEndpoint}\nFile Name:emergency-dashboard.js`);
+                }
+                return res.status(response.statusCode).json(body);
+            }
+        })
+    }
+
+    exports.getfistulaGraftSet = (req, res) => {
+        let mysapSSO2Value = decodeURI(req.cookies['MYSAPSSO2']);
+        let mySAPSSO2Cookie = 'MYSAPSSO2=' + decodeURI(mysapSSO2Value);
+        var j = request.jar();
+        var cookie = request.cookie('MYSAPSSO2' + '=' + mysapSSO2Value);
+        const urlEndpoint = String.raw`${baseURL}${config.apiFISTULAGRAFTSRV}/FistulaGraftSet?$filter=(Dockey='${req.query.Dockey}')&$format=json`
+        request({
+            method: 'GET',
+            uri:`${urlEndpoint}`,
+            body: req.body,
+            json: true,
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'X-Requested-With': 'XMLHttpRequest',
+                'sap-client': config.client,
+                'Cookie': mySAPSSO2Cookie,
+    
+                //'Authorization': 'Basic cmFrc2hpdGQ6aWRoYUAxMjM=',
+            }
+        }, function (error, response, body) {
+            if (error) {
+                res.json(error);
+                return console.dir(error);
+            }
+            else {
+                res.header('Access-Control-Allow-Origin', config.AllowOriginDomain);
+                res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+                res.header('Access-Control-Expose-Headers', 'Content-Length');
+                res.header('Access-Control-Allow-Credentials', 'true');
+                res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Origin,sap-client,Accept, Authorization, Content-Type, X-Requested-With, Range,Access-Control-Allow-Credentials');
+                if(response.statusCode != 200){
+                      logger.log('error', `Status Code: ${response.statusCode}\nBody: ${body}\nURL Endpoint: ${urlEndpoint}\nFile Name:emergency-dashboard.js`);
+                    }
+                return res.status(response.statusCode).json(body);
+            }
+        })
+    }
+
+    exports.latestDocSet = (req, res) => {
+        let mysapSSO2Value = decodeURI(req.cookies['MYSAPSSO2']);
+        let mySAPSSO2Cookie = 'MYSAPSSO2=' + decodeURI(mysapSSO2Value);
+        var j = request.jar();
+        var cookie = request.cookie('MYSAPSSO2' + '=' + mysapSSO2Value);
+        const urlEndpoint = String.raw`${baseURL}${config.apiFISTULAGRAFTSRV}/LatestDocSet?$filter=Einri='${req.query.einri}' and Falnr = '${req.query.falnr}' and Patnr = '${req.query.patnr}' and Lfdnr = '${req.query.lfdnr}'&$format=json`
+        request({
+            method: 'GET',
+            uri:`${urlEndpoint}`,
+            body: req.body,
+            json: true,
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'X-Requested-With': 'XMLHttpRequest',
+                'sap-client': config.client,
+                'Cookie': mySAPSSO2Cookie,
+    
+                //'Authorization': 'Basic cmFrc2hpdGQ6aWRoYUAxMjM=',
+            }
+        }, function (error, response, body) {
+            if (error) {
+                res.json(error);
+                return console.dir(error);
+            }
+            else {
+                res.header('Access-Control-Allow-Origin', config.AllowOriginDomain);
+                res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+                res.header('Access-Control-Expose-Headers', 'Content-Length');
+                res.header('Access-Control-Allow-Credentials', 'true');
+                res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Origin,sap-client,Accept, Authorization, Content-Type, X-Requested-With, Range,Access-Control-Allow-Credentials');
+                if(response.statusCode != 200){
+                      logger.log('error', `Status Code: ${response.statusCode}\nBody: ${body}\nURL Endpoint: ${urlEndpoint}\nFile Name:emergency-dashboard.js`);
+                    }
+                return res.status(response.statusCode).json(body);
+            }
+        })
+    }
+
+    exports.PDFFileSet = (req, res) => {
+        let mysapSSO2Value = decodeURI(req.cookies['MYSAPSSO2']);
+        let mySAPSSO2Cookie = 'MYSAPSSO2=' + decodeURI(mysapSSO2Value);
+        var j = request.jar();
+        var cookie = request.cookie('MYSAPSSO2' + '=' + mysapSSO2Value);
+        const urlEndpoint = String.raw`${baseURL}${config.apiFISTULAGRAFTSRV}/PDFFileSet(Dockey='${req.query.Dockey}')?$format=json`
+        request({
+            method: 'GET',
+            uri:`${urlEndpoint}`,
+            body: req.body,
+            json: true,
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'X-Requested-With': 'XMLHttpRequest',
+                'sap-client': config.client,
+                'Cookie': mySAPSSO2Cookie,
+    
+                //'Authorization': 'Basic cmFrc2hpdGQ6aWRoYUAxMjM=',
+            }
+        }, function (error, response, body) {
+            if (error) {
+                res.json(error);
+                return console.dir(error);
+            }
+            else {
+                res.header('Access-Control-Allow-Origin', config.AllowOriginDomain);
+                res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+                res.header('Access-Control-Expose-Headers', 'Content-Length');
+                res.header('Access-Control-Allow-Credentials', 'true');
+                res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Origin,sap-client,Accept, Authorization, Content-Type, X-Requested-With, Range,Access-Control-Allow-Credentials');
+                if(response.statusCode != 200){
+                      logger.log('error', `Status Code: ${response.statusCode}\nBody: ${body}\nURL Endpoint: ${urlEndpoint}\nFile Name:emergency-dashboard.js`);
+                    }
+                return res.status(response.statusCode).json(body);
+            }
+        })
+    }
+
+    exports.deleteFistulaGraftSet = (req, res) => {
+        let mysapSSO2Value = decodeURI(req.cookies['MYSAPSSO2']);
+        let mySAPSSO2Cookie = 'MYSAPSSO2=' + decodeURI(mysapSSO2Value);
+    
+        var j = request.jar();
+        var cookie = request.cookie('MYSAPSSO2' + '=' + mysapSSO2Value);
+        request({
+            method: 'DELETE',
+            uri: baseURL + config.apiFISTULAGRAFTSRV + `/FistulaGraftSet(Dockey='${req.query.Dockey}')`,
+            json: true,
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'X-Requested-With': 'XMLHttpRequest',
+                'sap-client': config.client,
+                'Cookie': mySAPSSO2Cookie,
+    
+                //'Authorization': 'Basic cmFrc2hpdGQ6aWRoYUAxMjM=',
+            }
+        }, function (error, response, body) {
+            if (error) {
+                res.json(error);
+                return console.dir(error);
+            }
+            else {
+                res.header('Access-Control-Allow-Origin', config.AllowOriginDomain);
+                res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+                res.header('Access-Control-Expose-Headers', 'Content-Length');
+                res.header('Access-Control-Allow-Credentials', 'true');
+                res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Origin,sap-client,Accept, Authorization, Content-Type, X-Requested-With, Range,Access-Control-Allow-Credentials');
+                if(response.statusCode != 200){
+                      logger.log('error', `Status Code: ${response.statusCode}\nBody: ${body}\nURL Endpoint: ${urlEndpoint}\nFile Name:emergency-dashboard.js`);
+                }
+                return res.status(response.statusCode).json(body);
+            }
+        })
+    }
 }
