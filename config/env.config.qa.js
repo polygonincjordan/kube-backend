f8efe1aaf1b1fe709a871a8bfffa09c54f4d59c8
Chintan Parikh <chintan@dynamicsats.onmicrosoft.com>
2024-07-04 00:01:28 +0530

    Sync Changes

--- config/env.config.qa.js
+++ config/env.config.qa.js
@@ -89,6 +89,7 @@ module.exports = {
     "apiZNPAINASSESSRV":"ZN_PAIN_ASSES_SRV",
     "apiFISTULAGRAFTSRV":"ZN_FISTULA_GRAFT_SRV",
     "apiZNPATIENTSEARCHSRV": "ZN_PATIENT_SEARCH_SRV",
+    "apiZNRESERVATION":"ZN_RESERVATION_SRV",
     "apiDomain": "ach.jo",
     "AllowOriginDomain": "http://abdaliwebserver.ach.jo:8090",
     "environment": "qa",
