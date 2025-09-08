4bad8855cd9de443e84d08ddc4cd83ab5b5ed61d
Jaydeep <jaydeep.jv@outlook.com>
2025-09-08 18:39:18 +0530

    Implemented FillSourceSet and EventChangeLogListSet APIs for the e-prescription

--- src/routes/e-prescription-data.js
+++ src/routes/e-prescription-data.js
@@ -1721,6 +1721,85 @@ router.put("/updateFillSource/", async (req, res) => {
     })
 });
 
+router.post("/updateFillSourcepost/", async (req, res) => {
+    const urlEndpoint = `${config.apiEndpointIntegrationAdminister}/FillSourceSet`;
+    console.log(urlEndpoint)
+
+    let mysapSSO2Value = decodeURI(req.cookies['MYSAPSSO2']);
+    let mySAPSSO2Cookie = 'MYSAPSSO2=' + decodeURI(mysapSSO2Value);
+    request({
+        method: 'POST',
+        uri: urlEndpoint,
+        body: req.body,
+        json: true,
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Requested-With': 'XMLHttpRequest',
+            'sap-client': config.client,
+            'Cookie': mySAPSSO2Cookie,
+        }
+    }, function (error, response, body) {
+        console.log(response);
+        console.log(JSON.stringify(body));
+        if (error) {
+            logger.log('error',error.message);
+            res.json(error);
+            return console.dir(error);
+        }
+        else {
+            if(response.statusCode != 200){
+            logger.log('error', `Status Code: ${response.statusCode}\nBody: ${body}\nURL Endpoint: ${urlEndpoint}\nFile Name:e-prescription-data.js`);
+            }
+            return res.status(response.statusCode).json(body);
+        }
+    })
+});
+
+router.get("/EventChangeLogListSet/", (req, res) => {
+    let mysapSSO2Value = decodeURIComponent(req.cookies['MYSAPSSO2']);
+    let mySAPSSO2Cookie = 'MYSAPSSO2=' + decodeURI(mysapSSO2Value);
+    var j = request.jar();
+    var cookie = request.cookie('MYSAPSSO2' + '=' + mysapSSO2Value);
+    j.setCookie(cookie, config.apiEndpoint, { domain: config.apiDomain });
+    const { Meevtid } = req.query;
+    const urlEndpoint = String.raw`${config.apiEndpointIntegrationOrderdetails}/EventChangeLogListSet?$filter=Meevtid eq '${Meevtid}'&$format=json`;
+    const options = {
+        url: `${urlEndpoint}`,
+        headers: {
+            'User-Agent': 'request',
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Cookie': mySAPSSO2Cookie,
+            'sap-client': config.client
+        },
+        jar: j
+    };
+
+    request.get(options, (error, response, body) => {
+        if (error) {
+            logger.log('error',error.message);
+            res.json({ message: err });
+            return console.dir(error);
+        }
+        else {
+            res.header('Access-Control-Allow-Origin', config.AllowOriginDomain);
+            res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+            res.header('Access-Control-Expose-Headers', 'Content-Length');
+            res.header('Access-Control-Allow-Credentials', 'true');
+            res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Origin,sap-client,Accept, Authorization, Content-Type, X-Requested-With, Range,Access-Control-Allow-Credentials');
+            if(response.statusCode != 200){
+            logger.log('error', `Status Code: ${response.statusCode}\nBody: ${body}\nURL Endpoint: ${urlEndpoint}\nFile Name:e-prescription-data.js`);
+            }
+            if (response.statusCode == 401) {
+
+                return res.status(response.statusCode).json(body);
+            }
+            else {
+                return res.status(response.statusCode).json(JSON.parse(body));
+            }
+        }
+    });
+});
 
 router.get("/FSourcelist/", (req, res) => {
     let mysapSSO2Value = decodeURIComponent(req.cookies['MYSAPSSO2']);
